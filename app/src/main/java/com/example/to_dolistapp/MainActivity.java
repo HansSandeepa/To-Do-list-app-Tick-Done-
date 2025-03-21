b1a604adc762b5df7e41642d6cc9b55e41688423
Hansana Sandeepa <151901922+HansSandeepa@users.noreply.github.com>
2025-03-21 12:23:48 +0530

    created a add task model

--- app/src/main/java/com/example/to_dolistapp/MainActivity.java
+++ app/src/main/java/com/example/to_dolistapp/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.to_dolistapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.to_dolistapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    Button addTaskBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // get xml components from binding class
+        addTaskBtn = binding.addTaskBtn;
+
+    }
+
+    private void showAddTaskModel(){
+        addTaskBtn.setOnClickListener(v -> {
+
         });
     }
 }
