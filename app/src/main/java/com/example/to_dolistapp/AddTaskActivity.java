d8faf9910093228b0f452c65494e2f4db141a512
Hansana Sandeepa <151901922+HansSandeepa@users.noreply.github.com>
2025-04-17 18:45:48 +0530

    insert data and setup validation

--- app/src/main/java/com/example/to_dolistapp/AddTaskActivity.java
+++ app/src/main/java/com/example/to_dolistapp/AddTaskActivity.java
@@ -5,15 +5,25 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.to_dolistapp.databinding.AddTaskBinding;
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddTaskActivity extends AppCompatActivity {
 
     //declare binding object
     private AddTaskBinding binding;
+    String taskName, selectedDate, selectTime;
     TextView selectedDateView,selectedTimeView,standardTimeView;
+
+    boolean isTimeBtnSelected = false,isDateBtnSelected = false;    //used to check if date and time buttons are even clicked once
+    boolean isTimeSelected = false,isDateSelected = false;    //used to check if date and time dialogs are canceled without selecting a data
+    String taskNameRegex = ".{0,100}$"; //regex for validation
+    private TasksDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -30,11 +40,17 @@ public class AddTaskActivity extends AppCompatActivity {
         selectedTimeView = binding.selectedTime;    //this is used for human readable time format (hh:mm a)
         standardTimeView = binding.standardTimeContainer;   //this is used for standard time format (HH:mm)
 
+        //initialize the database
+        db = new TasksDatabase(AddTaskActivity.this);
+
         //show date picker
         datePicker();
 
         //show time picker
         timePicker();
+
+        //save Tasks
+        saveTask();
     }
 
     //date picker method
@@ -64,9 +80,24 @@ public class AddTaskActivity extends AppCompatActivity {
                         }
                         Log.i("SELECTED DATE: ",date);
                         selectedDateView.setText(date);
+                        isDateSelected = true;
                     },
                     year, month,day
                     );
+
+            //set minimum date to today
+            datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+
+            //check if date dialog is canceled without selecting a date
+            datePickerDialog.setOnDismissListener(dialog -> {
+                        if (!isDateSelected) {
+                            binding.dateValidation.setText(R.string.please_select_a_date);
+                            isDateBtnSelected = false;
+                        }else {
+                            binding.dateValidation.setText("");
+                            isDateBtnSelected = true;
+                        }
+                    });
             datePickerDialog.show();
         });
     }
@@ -78,6 +109,8 @@ public class AddTaskActivity extends AppCompatActivity {
         int min = calendar.get(Calendar.MINUTE);
 
         binding.buttonSelectTime.setOnClickListener(v -> {
+            binding.timeValidation.setText(""); //clear error message
+
             TimePickerDialog timePickerDialog = new TimePickerDialog(
                     this,
                     (view,hourOfDay,minute) ->{
@@ -95,14 +128,112 @@ public class AddTaskActivity extends AppCompatActivity {
                                 selectedTime = hourOfDay + ":" + minute;
                             }
                         }
-                        standardTimeView.setText(selectedTime);
+                        standardTimeView.setText(selectedTime); //
                         Log.i("SELECTED TIME: ",selectedTime);
                         String convertedTime = new TimeConversion().convertedTime(selectedTime); //convert time into human readable format
                         selectedTimeView.setText(convertedTime);
+                        isTimeSelected = true;
                     },
                     hour,min,false
             );
+
+            //check if time dialog is canceled without selecting a time
+            timePickerDialog.setOnDismissListener(dialog -> {
+                if (!isTimeSelected){
+                    binding.timeValidation.setText(R.string.please_select_a_time);
+                    isTimeBtnSelected = false;
+                }else{
+                    isTimeBtnSelected = true;
+                    binding.timeValidation.setText("");
+                }
+            });
+
             timePickerDialog.show();
         });
+    }
+
+    //save button action
+    private void saveTask(){
+        binding.buttonSaveTask.setOnClickListener(v -> {
+            getData();  //get data from the inputs
+
+            //validate data
+            if (insertValidation()){
+                if (db.addTask(taskName,selectedDate, selectTime)){
+                    Toast.makeText(AddTaskActivity.this, "Task added successfully!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(AddTaskActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //validate Data
+    private boolean insertValidation(){
+        Log.i("Task name: ",taskName);
+
+        //task name validation
+        if (taskName.isEmpty()){
+            binding.taskNameField.setError("Task name cannot be empty!");
+            return false;
+        } else if (regexCheck(taskName, taskNameRegex)) {
+            binding.taskNameField.setError("Task must have less that 100 letters!");
+            return false;
+        }
+
+        //date validation
+        if(!isDateBtnSelected){
+            binding.dateValidation.setText(R.string.please_select_a_date);
+            return false;
+        } else if (!isDateSelected) {
+            binding.dateValidation.setText(R.string.please_select_a_date);
+            return false;
+        }
+
+        //time validation
+        if (!isTimeBtnSelected){
+            binding.timeValidation.setText(R.string.please_select_a_time);
+            return false;
+        } else if (!isTimeSelected) {
+            binding.timeValidation.setText(R.string.please_select_a_time);
+            return false;
+        }
+        return true;
+    }
+
+    //update validation
+    private boolean updateValidation(){
+        boolean haveData = false;
+
+        //empty check
+        if (taskName.isEmpty()){
+            binding.taskNameField.setError("Task name cannot be empty!");
+            return false;
+        }
+
+        //task name regex check
+        if (regexCheck(taskName, taskNameRegex)) {
+            binding.taskNameField.setError("Task must have less that 100 letters!");
+            return false;
+        }
+        return true;
+    }
+
+
+    //regex pattern compilation
+    private boolean regexCheck(String input,String regex){
+        // Compile the pattern
+        Pattern pattern = Pattern.compile(regex);
+        // Create a matcher for the input string
+        Matcher matcher = pattern.matcher(input);
+        // Check if the input string matches the pattern
+        return !matcher.matches();
+    }
+
+    //getData from the inputs
+    private void getData(){
+        taskName = binding.taskNameField.getText().toString();
+        selectedDate = binding.selectedDate.getText().toString();
+        selectTime = binding.standardTimeContainer.getText().toString();
     }
 }
